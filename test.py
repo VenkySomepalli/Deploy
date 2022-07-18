2ceecfa13279df56289049174b04a5c284ac08ae
VenkySomepalli <106543953+VenkySomepalli@users.noreply.github.com>
2022-07-18 09:39:02 +0530

    Update test.py

--- test.py
+++ test.py
@@ -131,7 +131,7 @@ def main():
     # front end elements of the web page 
     html_temp = """ 
     <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit Price Prediction of EdTech product ML App</h1> 
+    <h1 style ="color:black;text-align:center;">Price Prediction of an EdTech Product </h1> 
     </div> 
     """
       
